65dd08e9d0248b04b1b14249100e829fcda13d13
FranciscoGomes20 <henriquecgomes10@gmail.com>
2023-12-20 16:41:40 -0500

    Update in code

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,8 +10,8 @@
          <div class="flex min-w-0 gap-x-4">
            <img class="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
            <div class="min-w-0 flex-auto">
-             <p class="text-sm font-semibold leading-6 text-gray-900">{{ product.title }}</p>
-             <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ product.content }}</p>
+             <p class="text-sm font-semibold leading-6 text-gray-900">{{ product.nome }}</p>
+             <p class="mt-1 truncate text-xs leading-5 text-gray-500">{{ product.valor }}</p>
            </div>
          </div>
          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
@@ -29,14 +29,14 @@
 
 import axios from "axios";
 
-console.log(axios.get('posts'))
+
 import {onMounted, ref} from "vue";
 
 const products = ref([]);
 
 
 onMounted(() => {
-    axios.get('posts')
+    axios.get('produtos')
         .then((response) => {
             products.value = response.data;
         })
